22cf16f6841b6d8174480fdbaa305445f51d1b46
tomas_mokry <tomas.mokry@gmail.com>
2022-11-14 20:44:14 +0100

    first commit

---
+++ projekt_2A.py
@@ -0,0 +1,221 @@
+"""
+projekt_2A.py: druhý projekt do Engeto Online Python Akademie
+author: Tomáš Mokrý
+email: tomas.mokry@gmail.com
+discord: Tomas M#0922
+
+"""
+import time
+from random import randint
+
+def main():
+    game_on = True
+    game(game_on)
+    
+
+def game(game_on):
+    
+    welcome_text()
+    number_comp = generate_number()
+    rounds = 1
+    start_time = time.time()
+    
+    while game_on:
+        
+        print(f'({number_comp})') 
+        number_user = input('>>> ')
+        warning = 'Wrong entry, try again.'
+        
+        if not number_user.isnumeric():
+            print(warning)
+            continue
+        
+        elif number_user.startswith('0'):
+            print(warning)
+            continue
+        
+        elif len(number_user) != 4:
+            print(warning)
+            continue
+        
+        elif find_duplicates(number_user) == False:
+            print(warning)
+            continue
+        
+        elif number_user == number_comp:
+            game_on = False
+        
+        else: 
+            show_game_result(find_bulls_cows(number_comp, number_user))
+            rounds += 1
+        
+    else:
+        evaluate_result(rounds)
+        game_time = time.time() - start_time
+        print(
+            'Your game time:', 
+            time.strftime("%H:%M:%S",time.gmtime(game_time))
+            )
+        name_pl = input('Please enter your name for statistics: ')
+        s= f"Name: {name_pl}, gueses: {rounds}, time: {round(game_time,1)} s\n"
+        with open("statistics.txt", mode='a') as stat_file:
+            stat_file.write( s )
+        print('Find your game staistics in file statistics.txt')
+        
+        
+def find_bulls_cows(number_comp: str, number_user: str) -> list:
+    """
+    Description:
+    Takes 2 numbers and checks same numbers positioned on same indexes
+    and checks same numbers in each other.
+    
+    Example: 
+    number_comp = '1234'
+    number_user = '1432'
+
+    Result:
+    ---------
+    [1,4]
+
+    1 - one same number on same indexes
+    4 - four same numbers in numbers
+    ---------
+    """
+    bulls = 0
+    cows = 0
+    for i,j in enumerate(number_comp):
+        if j == number_user[i]:
+            bulls += 1
+        if j in number_user:
+            cows += 1
+    return [bulls, cows]
+
+
+def show_game_result(results: list) -> None:
+    """
+    Description:
+    Takes result list [x,y]. 
+    According to the number will asign name Bull,Cow for number == 1
+    Bulls, Cows for number > 1 nad number 0
+    Prints results 
+
+    Example: 
+    x = 3
+    y = 1
+    
+    Result:
+    ---------
+    3 Bulls, 1 Cow
+    ---------
+    """ 
+    separator = 48 * '-'
+    name_1 = "Bulls"
+    name_2 = "Cows"
+    
+    if results[0] == 1:
+        name_1 = "Bull"
+    if results[1] == 1:
+        name_2 = "Cow"
+
+    print(
+        f'{results[0]} {name_1}, {results[1]} {name_2}', 
+        separator, sep="\n"
+        )
+
+
+def welcome_text() -> None:
+    """
+    Description:
+    Prints the welcome text
+
+    """
+    separator = 48 * '-'
+    print(
+        separator,
+        "Hi there!",
+        separator,
+        "I've generated a random 4 digit number for you.",
+        "Let's play a bulls and cows game.",
+        separator, 
+        "Enter a number: ",
+        separator, sep = "\n"
+        ) 
+
+
+def generate_number() -> str:
+    """
+    Description:
+    Generates 4 digit random number. Each digit is generated separately
+    and added to the list. List is joined to string 4 digit number.
+    
+    Example: 
+    number_generated = ["1", "5", "6", "9"]
+
+    Result:
+    ---------
+    "1569"
+    ---------
+    """ 
+    
+    number_generated = []
+    while len(number_generated) < 4:
+    
+        if (num := str(randint(1,9))) not in number_generated:
+            number_generated.append(num)
+
+    return ''.join(number_generated)
+    
+    
+def evaluate_result(rounds: int) ->None:
+    """
+    Takes value of quesses and compare with limits.
+    < 5; 5 < rounds < 10; rounds > 10
+    Prints result
+
+    Example: 
+    rounds = 11
+
+    Result:
+    ---------
+    evaluation = "That is not so good..." 
+    ---------
+    """ 
+    separator = 48 * '-'
+    
+    if rounds < 5:
+        evaluation = "That is amazing!"
+    elif 5 < rounds < 10:
+        evaluation = "That is average"
+    else:
+        evaluation = "That is not so good..." 
+    
+    print(
+        f"Correct, you've guessed the right number \nin {rounds} guesses!",
+    separator,evaluation,separator, sep = '\n'
+    )
+
+def find_duplicates(number_user:str) ->bool:
+    """
+    Takes one value, checking if there are duplicates in the value
+    
+    Example: 
+    number_user = 1234
+    number_user = 1222
+    
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """ 
+    while int(number_user) > 0:
+        rest = int(number_user) % 10
+        number_user = int(number_user) // 10
+        if str(rest) in str(number_user):
+            return False
+            break
+    else:
+        return True
+
+if __name__ == "__main__":
+    main()
---
+++ projekt_2B.py
@@ -0,0 +1,334 @@
+"""
+projekt_2B.py: druhý projekt do Engeto Online Python Akademie
+author: Tomáš Mokrý
+email: tomas.mokry@gmail.com
+discord: Tomas M#0922
+
+"""
+import time
+import os
+from random import randint
+
+
+def main():
+    game_on = True
+    game(game_on)
+
+def game(game_on):
+
+    start_time = time.time()
+    field = create_game_board()
+    player = ' X ' if random_first_player() == 1 else ' O '
+    
+    while game_on:
+        os.system('clear')
+        divider = 48 * '='
+        welcome_text()
+        show_board(field)
+        
+        position = input(f'Player {player} | Please enter your move number: ')
+        if not position.isnumeric():
+            print('Wrong input, please try again')
+            time.sleep(1)
+            continue
+        elif len(position) != 1:
+            print('Wrong input, please try again')
+            time.sleep(1)
+            continue
+        elif field[(int(position)-1) // 3][(int(position)-1) % 3] != '   ':
+            print('This position is already taken, please try again.')
+            time.sleep(1)
+            continue
+        else:
+            position = int(position)
+        
+        field = write_player_move(field, player, position)
+        
+        if find_winner(field, player):
+            game_on = False
+        
+        if is_field_full(field):
+            game_on = False
+        
+        player = switch_players(player)
+    
+    else:
+        os.system("clear")
+        game_time = time.time() - start_time
+        
+        if is_field_full(field):
+            print(f'It is a GAME DRAW, play again. \n{divider}')
+        else:
+            player = switch_players(player)
+            print(f'Player {player} is the WINNER! Congratulation! \n{divider}')
+        
+        show_board(field)
+        print(
+            divider,
+            'Your game time:', time.strftime("%H:%M:%S",time.gmtime(game_time)),
+            divider,
+            sep = '\n'
+            )
+        
+
+def random_first_player() ->int:
+    """
+    Description:
+    Generates random integer 0 or 1 
+    
+    """
+    return randint(0,1) 
+
+
+def switch_players(player: str) ->str:
+    """
+    Description:
+    Takes one string name of one player 
+    and return string name of second player
+    
+    Example: 
+    player = ' X '
+    player = ' O '  
+    
+    Result:
+    ---------
+    ' O '
+    ' X '
+    ---------
+    """
+    return ' X ' if player == ' O ' else ' O '
+
+
+def create_game_board() ->list:
+    """
+    Description:
+    Creates nested list, 
+    
+    Result:
+    ---------
+    [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+    ---------
+    """
+    board = []
+    for i in range(3):
+        row=[]
+        for j in range(3):
+            row.append('   ')
+        board.append(row)
+    return board
+
+
+def show_board(field: list) ->None:
+    """
+    Description:
+    Takes list and print game board
+
+    Example: 
+    field = [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+
+    Result:
+    ---------
+    +-----+-----+-----+
+    |     |     |     |
+    +-----+-----+-----+
+    |     |     |     |
+    +-----+-----+-----+
+    |     |     |     |
+    +-----+-----+-----+
+    ---------
+    """
+    for row in field:
+        print('+-----+-----+-----+')
+        for item in row:
+            print('|', item, end=" ")
+        print('|')
+    print('+-----+-----+-----+')
+
+
+def welcome_text() ->None:
+    """
+    Description:
+    Prints the welcome text
+
+    """
+    divider = 48 * '='
+    divider_2 = 48 * '-'
+    print(f'''
+Welcome to Tic Tac Toe
+{divider}
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row
+{divider}
+Let's start the game
+{divider_2}'''
+) 
+
+
+def write_player_move(field: list, player: str, position: int) ->list:
+    """
+    Description:
+    Placing the player symbol into the list into the position
+    input position number is from 1 - 9, this number
+    is translated into nested list indexes
+    
+    Example: 
+    position = 9
+    position = 7
+    
+    Result:
+    ---------
+    field[2][2] = player
+    field[2][0] = player
+    ---------
+    """
+    
+    index_1 = (position-1) // 3
+    index_2 = (position-1) % 3
+    field[index_1][index_2] = player
+    return field
+
+
+def is_field_full(field: list) ->bool:
+    """
+    Description:
+    Checking if all positions in the list are filled
+    if yes returns True
+    if not returns False
+    
+    Example: 
+    [[' X ',' O ',' X '],[' X ',' X ',' O '],[' O ',' X ',' X ']]
+    [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """ 
+    full = True
+    for i in range(3):
+        for j in range(3):
+            if field[i][j] == '   ':
+                full = False
+                break
+    return full
+
+
+def find_winner_rows(field: list, player: str) ->bool:
+    """
+    Description:
+    Checking if there are 3 same player symbols in nested list (rows)
+    
+    Example: 
+    [[' X ',' X ',' X '],['   ','   ','   '],['   ','   ','   ']]
+    [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """
+    
+    for i in range(3):
+        winner = True
+        for j in range(3):
+            if field[i][j] != player:
+                winner = False
+                break
+        if winner:
+            return winner
+    return False
+
+
+def find_winner_columns(field: list, player: str) ->bool:
+    """
+    Description:
+    Checking if there are 3 same player symbols in nested list (columns)
+    
+    Example: 
+    [[' X ','   ','   '],[' X ','   ','   '],[' X ','   ','   ']]
+    [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """
+    
+    for i in range(3):
+        winner = True
+        for j in range(3):
+            if field[j][i] != player:
+                winner = False
+                break
+        if winner:
+            return winner
+    return False
+
+
+def find_winner_diagonals(field: list, player: str) ->bool: 
+    """
+    Description:
+    Checking if there are 3 same player symbols in nested list (diagonals)
+    
+    Example: 
+    [[' X ','   ','   '],['   ',' X ','   '],['   ','   ',' X ']]
+    [['   ','   ','   '],['   ','   ','   '],['   ','   ','   ']]
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """  
+    
+    winner = True
+    for i in range(3):
+        if field[i][i] != player:
+            winner = False
+            break
+    if winner:
+        return winner
+     
+    winner = True
+    for i in range(3):
+        if field[i][abs(i-2)] != player:
+            winner = False
+            break
+    if winner:
+        return winner
+    return False
+
+
+def find_winner(field: list, player: int) ->bool:
+    """
+    Description:
+    Checking the combination of find winner functions
+    find_winner_diagonals, find_winner_columns, find_winner_rows
+    if just one is True - return True, else False
+
+    Example: 
+    win_row = True
+    win_coll = False
+    win_dia = False
+
+    win_row = False
+    win_coll = False
+    win_dia = False
+    Result:
+    ---------
+    True
+    False
+    ---------
+    """ 
+    win_row = find_winner_rows(field,player)
+    win_coll = find_winner_columns(field,player)
+    win_dia = find_winner_diagonals(field,player)
+    return win_row or win_coll or win_dia
+
+if __name__ == "__main__":
+    main()
